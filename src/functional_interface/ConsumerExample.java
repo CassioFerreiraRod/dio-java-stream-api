864255471519951c06d70ae547a498049d11ba4a
Cassio Ferreira <cassio1215@hotmail.com>
2023-09-26 19:33:04 -0300

    Exemplo de Consumer com lambda

--- src/functional_interface/ConsumerExample.java
+++ src/functional_interface/ConsumerExample.java
@@ -15,17 +15,15 @@ public class ConsumerExample {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         // Usar o Consumer com expressão lambda para imprimir números pares
-//        Consumer<Integer> imprimiNumeroPar = numero -> {
-//            if (numero % 2 == 0) {
-//                System.out.println(numero);
-//            }
-//        };
-
-        numeros.forEach(n -> {
-                if (n % 2 == 0) {
-                    System.out.println(n);
-                }
+        Consumer<Integer> imprimiNumeroPar = numero -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
             }
-        );
+        };
+
+        // Usar o Consumer para imprimir números pares no Stream
+        numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
     }
 }
